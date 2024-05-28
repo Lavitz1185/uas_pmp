4f2d9f7b48b3aee299064414607e5cda653e44c4
Lavitzz <allgaming1185@gmail.com>
2024-05-28 23:04:10 +0800

    done gak bang

--- baksoapp/lib/screens/home/views/details_screen.dart
+++ baksoapp/lib/screens/home/views/details_screen.dart
@@ -1,15 +1,15 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:pizza_repository/pizza_repository.dart';
+import 'package:pizza_repository/pizza_repository.dart';
 // import '../../../components/macro.dart';
 
-// class DetailsScreen extends StatelessWidget {
-//   final Pizza pizza;
-//   const DetailsScreen(this.pizza, {super.key});
+ class DetailsScreen extends StatelessWidget {
+   final Pizza pizza;
+   const DetailsScreen(this.pizza, {super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
 //       backgroundColor: Theme.of(context).colorScheme.background,
 //       appBar: AppBar(
 //         backgroundColor: Theme.of(context).colorScheme.background,
@@ -159,6 +159,6 @@
 //           ],
 //         ),
 //       ),
-//     );
-//   }
-// }
+     );
+   }
+ }
