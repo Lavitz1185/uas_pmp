04cf8d3dc0fa38cb27276766bae5884160132993
Lavitzz <allgaming1185@gmail.com>
2024-05-28 20:14:54 +0800

    done gak bang

---
+++ baksoapp/android/app/src/main/kotlin/com/example/baksoapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.baksoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- uas_pmp/lib/main.dart
+++ uas_pmp/lib/main.dart
@@ -1,13 +1,13 @@
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:uas_pmp/app.dart';
-// import 'package:uas_pmp/simple_bloc_observer.dart';
-// import 'package:user_repository/user_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uas_pmp/app.dart';
+import 'package:uas_pmp/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   Bloc.observer = SimpleBlocObserver();
-//   runApp(MyApp(FirebaseUserRepo()));
-// }
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+   Bloc.observer = SimpleBlocObserver();
+   runApp(MyApp(FirebaseUserRepo()));
+}
